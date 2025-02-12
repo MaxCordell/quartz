aecaa3df679d8480b6eb5b68be16e4a26dfd54d1
Makkusu <31745046+MaxCordell@users.noreply.github.com>
2025-02-11 18:30:59 -0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz.maxcordell.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
